e64a97fd1fbdb07b4ef9de0a27c2a6ff7b6ee719
Darshfb <mostafamahmoudaboads@gmail.com>
2024-12-29 13:00:22 +0200

    update

--- src/test/java/testcases/TestBase.java
+++ src/test/java/testcases/TestBase.java
@@ -24,16 +24,14 @@ import static drivers.DriverFactory.getNewInstance;
 import static drivers.DriverHolder.getDriver;
 import static drivers.DriverHolder.setDriver;
 import static pages.PageBase.quitBrowser;
+import static utilities.ExtentReporter.openReports;
 
 @Listeners(listeners.Listener.class)
 public class TestBase {
     static WebDriver driver;
     //    static WebDriver driver;
     protected Faker faker = new Faker();
-    // extend report
-    protected static ExtentSparkReporter htmlReporter;
-    protected static ExtentReports extent;
-    protected static ExtentTest test;
+
     private static String PROJECT_NAME = null;
     private static String PROJECT_URL = null;
 
@@ -45,23 +43,8 @@ public class TestBase {
     public void beforeSuite() throws Exception {
 //        File folder = new File(System.getProperty("user.dir") + "\\recordings");
 //        FileUtils.cleanDirectory(folder);
-        // TODO: initialize the HtmlReporter
-        htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/testReport.html");
-
-        //initialize ExtentReports and attach the HtmlReporter
-        extent = new ExtentReports();
-        extent.attachReporter(htmlReporter);
-
         setProjectDetails();
 
-        // initialize test
-        test = extent.createTest(PROJECT_NAME + " Test Automation Project");
-
-        //configuration items to change the look and fee add content, manage tests etc
-        htmlReporter.config().setDocumentTitle(PROJECT_NAME + " Test Automation Report");
-        htmlReporter.config().setReportName(PROJECT_NAME + " Test Report");
-        htmlReporter.config().setTheme(Theme.STANDARD);
-        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
     }
 
     private void setProjectDetails() throws IOException {
@@ -82,8 +65,6 @@ public class TestBase {
         setDriver(getNewInstance(browserName));
 
         driver = getDriver();
-
-        slowNetwork();
         driver.get(PROJECT_URL);
     }
 
@@ -94,36 +75,8 @@ public class TestBase {
 
     @AfterSuite
     public void afterSuite() throws Exception {
-        extent.flush();
+        openReports();
     }
 
-    @AfterMethod
-    public void getResult(ITestResult result) {
-        if (result.getStatus() == ITestResult.FAILURE) {
-            test.log(Status.FAIL, result.getName() + " failed with the following error: " + result.getThrowable());
-            Reporter.log("Failed to perform " + result.getName(), 10, true);
-        } else if (result.getStatus() == ITestResult.SUCCESS) {
-            test.log(Status.PASS, result.getName());
-            Reporter.log("Successfully perform " + result.getName(), 10, true);
-        } else {
-            test.log(Status.SKIP, result.getName());
-            Reporter.log("Skip " + result.getName(), 10, true);
-        }
-    }
-
-    private void slowNetwork(){
-        // Enable DevTools and simulate slow network conditions
-        DevTools devTools = ((ChromeDriver) driver).getDevTools();
-        devTools.createSession();
-
-        // Simulate network conditions: slow 3G network (latency, bandwidth)
-        devTools.send(Network.emulateNetworkConditions(
-                false, // Offline (set to false for testing with network)
-                300,  // Latency (ms)
-                300,    // Download throughput (200 KB/s)
-                200 * 1024,  // Upload throughput (200 KB/s)
-                Optional.of(ConnectionType.CELLULAR2G)  // Correct enum reference for 3G connection
-        ));
-    }
 
 }
